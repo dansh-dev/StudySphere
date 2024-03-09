b5190669413764027cfce12aa11e7b40d1b68866
dansh-dev <162729761+dansh-dev@users.noreply.github.com>
2024-03-09 03:34:14 +0100

    Updating settings

--- StudySphere/settings.py
+++ StudySphere/settings.py
@@ -169,7 +169,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "/core_study/static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 CHANNEL_LAYERS = {
