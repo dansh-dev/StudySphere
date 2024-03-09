8ed219fafde51aae3859ae119357aaf0689686ab
dansh-dev <162729761+dansh-dev@users.noreply.github.com>
2024-03-09 03:40:15 +0100

    Updating settings

--- StudySphere/settings.py
+++ StudySphere/settings.py
@@ -94,6 +94,9 @@ WSGI_APPLICATION = 'StudySphere.wsgi.application'
 ASGI_APPLICATION = 'StudySphere.routing.application'
 
 
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
 MEDIA_ROOT = 'images'
 MEDIA_URL = '/images/'
 
@@ -168,9 +171,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
-STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 CHANNEL_LAYERS = {
     'default' : {
